94cb3cd1aa95ce54a09b3497de034b53cdce6846
AndyJs <88842740+andyjavadams@users.noreply.github.com>
2021-08-14 05:38:06 +0700

    Okelah

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,27 +1,35 @@
-let fetch = require('node-fetch')
+let fs = require('fs')
 
-let timeout = 100000
-let poin = 4999
-let handler  = async (m, { conn, usedPrefix }) => {
+let timeout = 120000
+let poin = 5000
+let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (id in conn.caklontong) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
         throw false
     }
-    let res = await fetch('https://zahirr-web.herokuapp.com/api/kuis/caklontong?apikey=zahirgans')
-    let json = await res.json()
+    let res = JSON.parse(fs.readFileSync('./src/caklontong.json'))
+    let random = Math.floor(Math.random() * res.length)
+    let json = res[random]
+    let caption = `
+${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
+`.trim()
     conn.caklontong[id] = [
-      await conn.reply(m.chat, `Soal: *${json.result.soal}*\nTimeout: *${(timeout / 1000).toFixed(2)} detik*\nKetik *${usedPrefix}hint caklontong* untuk hint\nBonus: ${poin} XP`, m),
-      json, poin,
-      setTimeout(() => {
-        if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\n*${json.result.deskripsi}*`, conn.caklontong[id][0])
-        delete conn.caklontong[id]
-      }, timeout)
+        await conn.send2Button(m.chat, caption.trim(), '© A N D Y - B O T Z', 'Bantuim bang', '.calo', 'Nyerah bang', 'nyerah'),
+        json, poin,
+        setTimeout(() => {
+            if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.keterangan}`, conn.caklontong[id][0])
+            delete conn.caklontong[id]
+        }, timeout)
     ]
-  }
-  handler.help = ['caklontong']
-  handler.tags = ['game']
-  handler.command = /^caklontong/i
-  
-  module.exports = handler
+}
+handler.help = ['caklontong']
+handler.tags = ['game']
+handler.command = /^caklontong/i
+
+module.exports = handler
--- plugins/caklontong_hint.js
+++ plugins/caklontong_hint.js
@@ -3,10 +3,9 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.caklontong)) throw false
     let json = conn.caklontong[id][1]
-    let nya = json.result.jawaban
-    let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
-    m.reply('```' + nyanya + '```')
+    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.caklontong[id][0])
 }
-handler.command = /^clhint$/i
+handler.command = /^calo$/i
 handler.limit = true
 module.exports = handler
