d1af8275739b296b7a278c702311409144e3524d
AndyJs <88842740+andyjavadams@users.noreply.github.com>
2021-10-29 12:37:29 +0700

    Wuis

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,21 +1,25 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, args }) => {
-    heum = await fetch(`https://recoders-area.caliph.repl.co/api/loli`)
-    json = await heum.buffer())
-   conn.sendFile(m.chat, json, 'nulis', 'Lomlinya kak', m, false)
+const fetch = require('node-fetch')
+let fs = require("fs")
+let { MessageType } =require("@adiwajshing/baileys")
+let handler = async (m, { conn }) => {
+    let res = await fetch('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json')
+   let json = await res.json()
+   let { url } = json
+   const media = await conn.prepareMessage(m.chat, {url:`${url}`}, MessageType.image, { thumbnail: Buffer.alloc(0) })// change for file type
+   const buttons = [
+  {buttonId: '/loli', buttonText: {displayText: 'get again 🗿'}, type: 1},
+  {buttonId: '/donasi', buttonText: {displayText: 'donasi'}, type: 1}
+]
+const buttonMessage = {
+    contentText: "Done\nBantu donasinya pack buat ngaktifin bot :)",
+    footerText: 'wm andy',
+    buttons: buttons,
+    headerType: 4, // change for file type
+    imageMessage: media.message.imageMessage // change for file type
+}
+const sendMsg = await conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage, { quoted: m})
 }
 handler.help = ['loli']
 handler.tags = ['anime']
-handler.command = /^loli$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
+handler.command = /^loli/i
 module.exports = handler
