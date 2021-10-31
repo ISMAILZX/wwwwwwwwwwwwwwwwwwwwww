dbc084cf1d7d7bba3ad29fd87b36bf265f60f52d
AndyJs <88842740+andyjavadams@users.noreply.github.com>
2021-10-31 11:06:02 +0700

    Update

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+let fs = require('fs')
 global.DeveloperMode = 'false' //true Or false
 global.owner = ['6285795431803','6282164659362', '6282331033919'] // Put your number here
 global.mods = ['6282331033919','6285795431803','6282164659362']  // Want some help?
-global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) 
 /*global.prems = ['6282331033919','62895604506489',  '6285795431803','6282164659362','6285336621105','6285821918425','6285856430321','6281232373549','6282131587252','6283844878048','6283819290307','6285656620756','6282215157003','6285343876512','6287880900584','6282211224927',] // Premium user has unlimited limit*/
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -11,7 +12,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'ya gitu bang, harus beli',
+  'https://api.xteam.xyz': 'faisalkey',
   'https://api.zeks.xyz': 'apivinz',
 }
 
@@ -21,7 +22,6 @@ global.author ='+6282164659362'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
-let fs = require('fs')
 let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
