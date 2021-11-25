39b93c62df62e9f839ff21d88362b72f43f8c2dc
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-25 21:52:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('ANDY-BOTZ WhatsApp\nPRISENT', {
+CFonts.say('ISMAIL WhatsApp\nBOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
