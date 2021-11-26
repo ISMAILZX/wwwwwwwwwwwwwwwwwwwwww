ffeb7789a555f9ee78467109254b6137ee1a7ea2
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-26 16:44:31 +0700

    Update goprem.js

--- plugins/goprem.js
+++ plugins/goprem.js
@@ -2,7 +2,7 @@ const { MessageType } = require("@adiwajshing/baileys")
 let handler = async (m, { conn, usedPrefix }) => {
 let prem = `Mau premium bang?`.trim()
 
-await conn.send2Button(m.chat, prem.trim(), '© A N D Y - B O T Z', 'Iya', '.hooh', 'Engga', '.ndak', { sendEphemeral: true, quoted: m })}
+await conn.send2Button(m.chat, prem.trim(), '© Ismail Kurama', 'Iya', '.hooh', 'Engga', '.ndak', { sendEphemeral: true, quoted: m })}
 
 
 handler.help = ['goprem']
